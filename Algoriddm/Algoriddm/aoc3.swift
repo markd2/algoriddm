cd81b0caedb6b7636535568ac64ad24f6850b62a
Mark Dalrymple <markd@borkware.com>
2021-06-06 07:27:08 -0500

    Factors logic into helper function for part 2

--- Algoriddm/Algoriddm/aoc3.swift
+++ Algoriddm/Algoriddm/aoc3.swift
@@ -26,23 +26,35 @@ private func loadStuffs(_ filename: String) -> [String] {
 } // loadStuffs
 
 
-func aoc3_1() {
-    let stuff = loadStuffs("aoc3-data.txt")
-    var treeCount = 0
-    var horizontalPosition = 0
-
+func traverseStuffCountingTrees(_ stuff: [String], 
+                                deltaH: Int, deltaV: Int, deltaDawn: Int = 0) -> Int {
     // assumes uniform width of lines
     let width = stuff[0].count
+    var treeCount = 0
 
-    stuff.dropFirst().forEach { line in
-        // vertical change is implicit
-        horizontalPosition += 3
+    var horizontalPosition = 0
+
+    for index in stride(from: 1, to: stuff.count, by: deltaV) {
+        let line = stuff[index]
+
+        horizontalPosition += deltaH
         let here = line.intDig(horizontalPosition % width)
+
+        print("looking at line \(index)  hpos \(horizontalPosition)  mod \(horizontalPosition % width) got \(here)")
+
         if here == "#" {
             treeCount += 1
         }
     }
-    print("SUCCESS \(treeCount)")
+
+    return treeCount
+}
+
+func aoc3_1() {
+    let stuff = loadStuffs("aoc3-data.txt")
+    
+    let treeCount = traverseStuffCountingTrees(stuff, deltaH: 3, deltaV: 1)
+    print("found \(treeCount)")
 }
 
 func aoc3_2() {
